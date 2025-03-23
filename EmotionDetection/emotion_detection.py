ce8f153852de65d07a4af994364d882cbbe5ddcb
David-Arellano-Corona <davidarellanocii@gmail.com>
2025-03-22 21:21:58 -0400

    fix error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,13 +2,15 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    if not text_to_analyse:
-        return {"anger": None,"disgust": None,"fear": None,"joy": None,"sadness": None,"dominant_emotion":None}
-
+    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=obj, headers=headers )
+    status_code = response.status_code
+    if status_code == 400:
+        return {"anger": None,"disgust": None,"fear": None,"joy": None,"sadness": None,"dominant_emotion":None}
+    
     formatted = json.loads(response.text)
     emotions = get_emotions(formatted)
     dominant_emotion = get_dominant_emotion(emotions)
